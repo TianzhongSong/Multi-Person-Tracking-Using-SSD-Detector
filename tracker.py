de3d151dfd06fab287e748fdc366fec11e348ec8
Tianz <ssstz@outlook.com>
2018-02-23 14:34:47 +0800

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Tracker():
+    def __init__(self):
+        self.bbox = []
+        self.index = []
+        self.features_previous = []
+        self.features_current = []
+
+    def update(self):
+        self.features_previous = self.features_current
+
+    def match(self):
+        # 待更新
+        pass
