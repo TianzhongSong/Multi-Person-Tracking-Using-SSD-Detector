6fcb458066e08079c475c6630e1cc270eb2d56af
Tianz <ssstz@outlook.com>
2018-03-17 13:20:02 +0800

    Update get_features.py

--- get_features.py
+++ get_features.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from HOG import hog
+import numpy as np
 
 
-def Extract_feature(feature):
-    # 提取特征, 这部分暂时还没决定怎么做
+def Extract_feature(input_img):
+    feature = hog(input_img, all_norms=True)
+    feature = np.concatenate((feature[0], feature[1], feature[2], feature[3]), axis=0)
     return feature
